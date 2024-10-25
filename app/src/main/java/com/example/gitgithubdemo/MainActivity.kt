76626f8e4e2f441438bea38683b06567f5d6d5f9
Maaz_Sayyad <maaz.s@peocitsoft.com>
2024-10-25 11:56:37 +0530

    Sixth Commit for Test(Comments).

--- app/src/main/java/com/example/gitgithubdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitgithubdemo/MainActivity.kt
@@ -17,8 +17,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        println("This Is Contained in my Second Commit")
+        /**
+         * Commit - Commit when you do your changes and then you save that changes.
+         * Revert Commit - when you do commit but later you realize, it was wrong changes, then you can revert back commit.
+         * Checkout - when you want to switch between all your commits. as you checkout then HEAD will point to that commit on your checkout.
+         * Stash - stash is when you did changes but you dont want to commit it and keep this changes as it is, and want to switch to another commit
+         *          then you can stash it, it will be save and when you back to that previous commit then you can unStash it.
+         */
 
-        akshajksdhalhsd kasdjhasdh
+        println("This Is Contained in my Second Commit")
     }
 }
